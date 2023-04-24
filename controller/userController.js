db1fb9753dc819e7a34048347767fc0530f2486f
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-04-24 16:30:08 +0545

    Change password

--- controller/userController.js
+++ controller/userController.js
@@ -177,3 +177,23 @@ exports.getLoggedInUserDetails = BigPromise(async (req, res, next) => {
     })
 
 })
+
+exports.changePassword = BigPromise(async (req, res, next) => {
+
+    const userId = req.user.id
+
+    const user = await User.findById(userId).select("+password")
+
+    const isCorrectOldPassword = await user.isValidatedPassword(req.body.oldPassword)
+
+    if (!isCorrectOldPassword) {
+        return next(new CustomError("Old password is incorrect", 400))
+    }
+
+    user.password = req.body.password
+
+    await user.save()
+
+    cookieToken(user, res)
+
+})
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signup, login, logout, forgotPassword, passwordReset, getLoggedInUserDetails } = require("../controller/userController");
+const { signup, login, logout, forgotPassword, passwordReset, getLoggedInUserDetails, changePassword } = require("../controller/userController");
 const { isLoggedIn } = require("../middleware/user");
 const router = express.Router()
 
@@ -10,6 +10,7 @@ router.route("/logout").get(logout)
 router.route("/forgotpassword").post(forgotPassword)
 router.route("/password/reset/:token").post(passwordReset)
 router.route("/userdashboard").get(isLoggedIn, getLoggedInUserDetails)
+router.route("/password/update").post(isLoggedIn, changePassword)
 
 
 
